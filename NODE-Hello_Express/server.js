6fb6f91bc45a89fb6c0361c848b13d95d127258b
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-10-05 13:48:08 -0500

    .

--- NODE-Hello_Express/server.js
+++ NODE-Hello_Express/server.js
@@ -1,5 +1,15 @@
 var express = require("express");
 var app = express();
+// new code:
+var session = require('express-session');
+// original code:
+var app = express();
+  // require body-parser
+  var bodyParser = require('body-parser');
+  // use it!
+  app.use(bodyParser.urlencoded({extended: true}));
+// more new code:
+app.use(session({secret: 'codingdojorocks'}));  // string for encryption
 
 app.get('/', function(request, response)
 {
@@ -26,19 +36,27 @@ app.get("/users", function (request, response){
     response.render('users', {users: users_array});
 })
 
+
 // root route
 app.get('/', function (req, res){
     res.render('index', {title: "my Express project"});
   });
   // route to process new user form data:
   app.post('/users', function (req, res){
+    console.log("POST DATA \n\n", req.body)
     // code to add user to db goes here!
     // redirect the user back to the root route. 
     // All we do is specify the URL we want to go to:
     res.redirect('/');
   })
-  // require body-parser
-var bodyParser = require('body-parser');
-// use it!
-app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post('/users', function (req, res){
+    // set the name property of session.  
+    req.session.name = req.body.name;
+    console.log(req.session.name);
+    //code to add user to db goes here!
+    // redirect the user back to the root route. 
+    res.redirect('/');
+});
 
