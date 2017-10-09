df2cc781e636cb62aaffee9a6b9df654b249f1b8
Brooks Von Doom <jeffery.brooks@loop.colum.edu>
2017-10-09 00:05:45 -0500

    quoting done

--- MEAN-Quoting_Dojo/server.js
+++ MEAN-Quoting_Dojo/server.js
@@ -12,19 +12,55 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 // ====================================================================
 
-mongoose.connect('mongodb://localhost/basic_mongoose');
+mongoose.connect('mongodb://localhost/quotes');
 // ====================================================================
 
-app.get('/', function(req, res) {
+// var UserSchema = new mongoose.Schema({
+//     name: String
+//    })
+//    mongoose.model('User', UserSchema);
+//    var User = mongoose.model('User')
+
+
+
+var QuoteSchema = new mongoose.Schema({
+    name: String,
+    quote: String
+   })
+   mongoose.model('quotes', QuoteSchema); // We are setting this Schema in our Models as 'User'
+   var Quote = mongoose.model('quotes') // We are retrieving this Schema from our Models, named 'User'
+// ====================================================================
+   
+
+app.get('/', function(req, res) 
+{
     // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
     res.render('index');
 })
 
-app.post('/users', function(req, res) {
-    console.log("POST DATA", req.body);
-    // This is where we would add the user from req.body to the database.
-    res.redirect('/');
-})
+app.get('/ViewQuotes', function(req, res)
+    {
+        Quote.find({}, function(err, results)
+            {
+                if(err) 
+                    { 
+                    console.log(err); 
+                    }
+                res.render('quotes', { quotes: results });
+            });
+    });
+  
+app.post('/AddQuotes', function(req, res)
+    {
+        Quote.create(req.body, function(err)
+            {
+                if(err) 
+                    { 
+                        console.log(err); 
+                    }
+                    res.redirect('/ViewQuotes');
+            });
+    });
 // ====================================================================
 
 app.listen(8000, function() {
